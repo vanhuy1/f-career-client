df8d00b9a36935c4b607d3b81b4f3689628ffaa5
rkorkey <nttloc2102@gmail.com>
2025-05-12 16:29:53 +0700

    feat: view list job by user

---
+++ src/services/api/jobs/job-api.ts
@@ -0,0 +1,58 @@
+import { Job, JobListResponse, CreateJobReq, UpdateJobReq } from '@/types/Job';
+import { httpClient } from '@/utils/axios';
+import { RequestBuilder } from '@/utils/axios/request-builder';
+
+class JobService {
+  private rb = new RequestBuilder().setResourcePath('jobs');
+
+  /** GET /jobs?limit=&offset= */
+  async findAll(limit?: number, offset?: number): Promise<JobListResponse> {
+    const url = this.rb.buildUrl('');
+
+    console.log(limit, offset); // handle share page
+    return await httpClient.get<JobListResponse>({
+      url,
+      typeCheck: (data) => ({ success: true, data: data as JobListResponse }),
+    });
+  }
+
+  /** GET /jobs/:id */
+  async findOne(id: string): Promise<Job> {
+    const url = this.rb.buildUrl(id);
+    return await httpClient.get<Job>({
+      url,
+      typeCheck: (data) => ({ success: true, data: data as Job }),
+    });
+  }
+
+  /** POST /jobs */
+  async create(payload: CreateJobReq): Promise<Job> {
+    const url = this.rb.buildUrl();
+    return await httpClient.post<Job, CreateJobReq>({
+      url,
+      body: payload,
+      typeCheck: (data) => ({ success: true, data: data as Job }),
+    });
+  }
+
+  /** PATCH /jobs/:id */
+  async update(id: string, payload: UpdateJobReq): Promise<Job> {
+    const url = this.rb.buildUrl(id);
+    return await httpClient.patch<Job, UpdateJobReq>({
+      url,
+      body: payload,
+      typeCheck: (data) => ({ success: true, data: data as Job }),
+    });
+  }
+
+  /** DELETE /jobs/:id */
+  async delete(id: string): Promise<void> {
+    const url = this.rb.buildUrl(id);
+    await httpClient.delete<void>({
+      url,
+      typeCheck: (data) => ({ success: true, data: data }),
+    });
+  }
+}
+
+export const jobService = new JobService();
