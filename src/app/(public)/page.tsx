8bcfd39387b7249e8610d2f90d8015118686a98c
Huy <thichanpho13@gmail.com>
2025-07-17 23:38:48 +0700

    feat: warning to updated profile

--- src/app/(public)/page.tsx
+++ src/app/(public)/page.tsx
@@ -1,11 +1,18 @@
+'use client';
+
+import { useHasUpdated } from '@/hooks/use-hasUpdated';
 import JobCategories from '@/components/landing/job-categories';
 import JobBoard from '@/components/landing/job-board';
 import JobBoardS from '@/components/landing/job-boards';
 import { HeroSection } from '@/components/landing/hero-section';
+import WarningProfileUpdated from '@/components/landing/warning-profile-updated';
 
 export default function Home() {
+  const { hasUpdated, loading } = useHasUpdated();
+
   return (
     <>
+      {!loading && hasUpdated === false && <WarningProfileUpdated />}
       <HeroSection />
       <JobCategories />
       <JobBoard />
