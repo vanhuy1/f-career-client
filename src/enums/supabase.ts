dbdfbc02a8911f175a873d1c79afef825dae3c72
Huy <thichanpho13@gmail.com>
2025-07-22 16:55:56 +0700

    feat: logo for company

--- src/enums/supabase.ts
+++ src/enums/supabase.ts
@@ -2,9 +2,11 @@ export enum SupabaseBucket {
   UPLOADS = 'uploads',
   USER_SETTINGS = 'usersetting',
   CV_UPLOADS = 'cvuploads',
+  COMPANY_ASSETS = 'company-assets',
 }
 
 export enum SupabaseFolder {
   USER_SETTINGS = 'user-settings',
   application = 'cv-uploads',
+  COMPANY_LOGOS = 'company-logos',
 }
